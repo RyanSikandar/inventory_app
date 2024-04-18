a85893eb1de7aff9b658912b56f134c88bc5cc81
RyanSikandar <ryansikandar@gmail.com>
2024-04-19 02:42:42 +0500

    setup auth service and register user function

--- frontend/src/components/Sidebar/SidebarItem.js
+++ frontend/src/components/Sidebar/SidebarItem.js
@@ -1,72 +1,65 @@
-import React from 'react'
-import { MdKeyboardArrowRight } from 'react-icons/md'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
-const activeSublink = ({ isActive }) => (
-    isActive ? 'active' : 'link'
-)
+const activeLink = ({ isActive }) => (isActive ? "active" : "link");
+const activeSublink = ({ isActive }) => (isActive ? "active" : "link");
 
-const activeLink = ({ isActive }) => (
-    isActive ? 'active' : 'link'
+const SidebarItem = ({ item, isOpen }) => {
+    const [expandMenu, setExpandMenu] = useState(false);
 
-
-)
-const SidebarItem = ({ items, isOpen }) => {
-
-    const [expandMenu, setExpandMenu] = React.useState(false)
-    if (items.children) {
+    if (item.childrens) {
         return (
-
-            <div className={expandMenu ? "sidebar-item s-parent open" : "sidebar-item s-parent "}>
-                <div className='sidebar-title'>
+            <div
+                className={
+                    expandMenu ? "sidebar-item s-parent open" : "sidebar-item s-parent"
+                }
+            >
+                <div className="sidebar-title">
                     <span>
-                        {items.icon &&
-                            <div className='icon'>
-                                {items.icon}
-                            </div>}
-                        {
-                            isOpen && items.title
-                        }
+                        {item.icon && <div className="icon">{item.icon}</div>}
+                        {isOpen && <div>{item.title}</div>}
                     </span>
-                    <MdKeyboardArrowRight onClick={() => setExpandMenu(!expandMenu)} className='arrow-icon' />
-
+                    <MdKeyboardArrowRight
+                        size={25}
+                        className="arrow-icon"
+                        onClick={() => setExpandMenu(!expandMenu)}
+                    />
                 </div>
-                <div className='sidebar-content'>
-                    {items.children.map((child, index) => {
+                <div className="sidebar-content">
+                    {item.childrens.map((child, index) => {
                         return (
-                            <div className='s-child' key={index}>
+                            <div key={index} className="s-child">
                                 <NavLink to={child.path} className={activeSublink}>
-                                    <div className='sidebar-item'>
-                                        <div className='sidebar-title'>
-                                            {child.icon && <div className='icon'>{child.icon}</div>}
-                                            {isOpen && child.title}
+                                    <div className="sidebar-item">
+                                        <div className="sidebar-title">
+                                            <span>
+                                                {child.icon && <div className="icon">{child.icon}</div>}
+                                                {isOpen && <div>{child.title}</div>}
+                                            </span>
                                         </div>
                                     </div>
                                 </NavLink>
                             </div>
-                        )
+                        );
                     })}
-
                 </div>
-
             </div>
-        )
-    }
-    else {
+        );
+    } else {
         return (
-            <NavLink to={items.path} className={activeLink}>
-                <div className='sidebar-item s-parent'>
-                    <div className='sidebar-title'>
+            <NavLink to={item.path} className={activeLink}>
+                <div className="sidebar-item s-parent">
+                    <div className="sidebar-title">
                         <span>
-                        {items.icon && <div className='icon'>{items.icon}</div>}
-                        {isOpen && items.title}
+                            {item.icon && <div className="icon">{item.icon}</div>}
+                            {isOpen && <div>{item.title}</div>}
                         </span>
-                        
                     </div>
                 </div>
             </NavLink>
-        )
+        );
     }
-}
+};
 
-export default SidebarItem
+export default SidebarItem;
